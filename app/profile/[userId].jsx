47654978c372c89b035e4968acd4087963003f4f
Azhar Ismagulova <azhar.botbayeva@gmail.com>
2023-10-20 23:34:24 +0100

    feat:create profile page

---
+++ app/profile/[userId].jsx
@@ -0,0 +1,16 @@
+import { useRouter } from "next/router";
+
+const UserProfile = () => {
+  const router = useRouter();
+
+  return (
+    <>
+     {/* Header
+     Hero
+     Bio
+     Feed */}
+    </>
+   );
+}
+ 
+export default UserProfile;

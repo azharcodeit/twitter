62c6904c81c7aa0565caa2ef7a52042907ba563b
Azhar Ismagulova <azhar.botbayeva@gmail.com>
2023-11-08 21:40:28 +0000

    feat:bookmarked posts feed page

---
+++ app/actions/getBookmarkedPosts.js
@@ -0,0 +1,19 @@
+import prisma from "lib/prismadb";
+import { getCurrentUser } from "@app/actions/getCurrentUser";
+
+export async function getBookmarkedPosts() {
+  try {
+    const currentUser = await getCurrentUser();
+
+    const posts = prisma.post.findMany({
+      where: {
+        bookmarkedIds: {
+          hasSome: [currentUser?.id],
+        },
+      },
+    });
+    return posts;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
